ffe24022c527164783e39f5dba28665e91c04902
LiangqinRen <liangqinren@gmail.com>
2025-03-12 09:53:43 -0500

    try

--- football_main.c
+++ football_main.c
@@ -3,20 +3,5 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    int score;
-    while (1) {
-        printf("Enter 0 or 1 to STOP\nEnter the NFL score: ");
-        scanf("%d", &score);
-
-        // stop the program if less or equal than 1
-        if (score <= 1) {
-            printf("Program terminated.\n");
-            break;
-        }
-
-        // calls function
-        int result = count_combinations(score);
-        printf("%d\n", result);
-    }
     return 0;
 }
