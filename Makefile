ddb56a87d3b396769bdf6e4e272dcdb2a488fc0e
LiangqinRen <liangqinren@gmail.com>
2025-03-12 09:43:34 -0500

    try

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC := gcc
 
 football.exe: football.c football_main.c
-	gcc football.c football_main.c -o football.exe
+	$(CC) football.c football_main.c -o football.exe
 
 report.exe: # fill in files here
 	# fill in commands here
--- football_main.c
+++ football_main.c
@@ -1,5 +1,19 @@
 #include "football.h"
 
 int main(int argc, char *argv[]) {
+    int score;
+    while (1) {
+        printf("Enter 0 or 1 to STOP\nEnter the NFL score: ");
+        scanf("%d", &score);
+
+        // stop the program if less or equal than 1
+        if (score <= 1) {
+            printf("Program terminated.\n");
+            break;
+        }
+
+        // calls function
+        count_combinations(score);
+    }
     return 0;
 }
